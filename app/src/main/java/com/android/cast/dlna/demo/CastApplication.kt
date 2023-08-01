fd1c97bcae4ef7f9f9d006478b53901f5a213ec6
LIUWEI <wei.liu@neulion.com.cn>
2023-08-01 18:29:02 +0800

    添加新的截图

--- app/src/main/java/com/android/cast/dlna/demo/CastApplication.kt
+++ app/src/main/java/com/android/cast/dlna/demo/CastApplication.kt
@@ -25,7 +25,10 @@ const val castVideoMp4Url_3 = "https://video.699pic.com/videos/39/09/13/sUXhxQmp
 const val castVideoM3u8Url = "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8"
 const val castVideoLocalUrl = "http://172.16.2.37:8192/storage/emulated/0/DCIM/Camera/20230705_165729.mp4"
 
-data class VideoUrl(val url: String, val title: String)
+data class VideoUrl(private val _url: String, val title: String) {
+    val url: String
+        get() = "$_url?vt=2&ff=ts&qd_originate=sys&k_uid=5a044bd3dd2ba3264ed53b18c3587e1e110d&_lnt=0&ve=259f68115c5afddb6851e932d97f5570&bossStatus=0&qd_uri=dash&qd_p=2dfb694a&bid=300&px=0&tvid=2845783617240700&qd_vip=0&qd_time=1690884846692&code=2&src=02022001010024000000-04000000001010000000-01&tm=1690884798049&sgti=37_5a044bd3dd2ba3264ed53b18c3587e1e110d_1690884798049&vf=9ba57fb5f8de8512a6ae5687ed26ec76"
+}
 
 val videoUrlList = mutableListOf(
     VideoUrl(castVideoMp4Url_3, "20秒的短视频, mp4格式"),
